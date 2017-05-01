b3644cda83201c6d823f8f2ca1b14a64338dfeef
ant <anthonyjones@zipcoder3.local>
2017-05-01 15:41:23 -0400

    Fixed Rotate Array Class

--- src/main/java/com/anthony/rotatearray/RotateArrays.java
+++ src/main/java/com/anthony/rotatearray/RotateArrays.java
@@ -6,33 +6,35 @@ import java.util.ArrayList;
 /**
  * Created by anthonyjones on 5/1/17.
  */
-public class RotateArrays extends ArrayList {
+public class RotateArrays extends List {
 
     ArrayList<Integer> list = new ArrayList<Integer>(7);
-
 
     public static void main(String[] args) {
         new RotateArrays();
     }
 
     RotateArrays() {
-        int listLength;
-        //int listOriginalLength = list.toArray().length;
+
+        System.out.println(rotateList(list, 2));
+    }
+
+    public ArrayList rotateList(ArrayList<Integer> list, int limitLoopToThisNumber) {
+
         for (int i = 0; i < 6; i++) {
             list.add(i, i + 1);
         }
-
         System.out.println(list);
-        listLength = list.toArray().length;
 
-        for (int i = 0, j = 3; i < listLength && j < 8; i++, j++) {
-            if (j >= 7) {
-                j = 1;
-            }
-            list.set(i, j);
+        for (int i = 0, j = 0; i < 6 && j < limitLoopToThisNumber; i++, j++) {
+            list.add(list.size(), list.get(j));
         }
-        System.out.println(list);
-    }
 
+        for (int i = 0, j = 0; i < limitLoopToThisNumber && j < 1; i++) {
+            list.remove(j);
+        }
+
+        return list;
+    }
 
 }
