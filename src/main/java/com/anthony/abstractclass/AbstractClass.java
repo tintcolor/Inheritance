113cd941c9e3ed465bf7a3682ca78285173ab58a
ant <anthonyjones@zipcoder3.local>
2017-05-01 13:20:46 -0400

    Kinda finished RotateArray class

---
+++ src/main/java/com/anthony/abstractclass/AbstractClass.java
@@ -0,0 +1,7 @@
+package com.anthony.abstractclass;
+
+/**
+ * Created by anthonyjones on 5/1/17.
+ */
+public class AbstractClass {
+}
---
+++ src/main/java/com/anthony/rotatearray/RotateArrays.java
@@ -0,0 +1,40 @@
+package com.anthony.rotatearray;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * Created by anthonyjones on 5/1/17.
+ */
+public class RotateArrays extends List {
+
+    ArrayList<Integer> list = new ArrayList<Integer>(7);
+
+
+    public static void main(String[] args) {
+        new RotateArrays();
+    }
+
+    RotateArrays() {
+        int listLength;
+        //int listOriginalLength = list.toArray().length;
+        for (int i = 0; i < 6; i++) {
+            list.add(i, i + 1);
+        }
+
+        System.out.println(list);
+        listLength = list.toArray().length;
+
+        for (int i = 0, j = 3; i < listLength && j < 8; i++, j++) {
+            if (j >= 7) {
+                j = 1;
+            }
+            list.set(i, j);
+        }
+        System.out.println(list);
+
+
+    }
+
+
+}
